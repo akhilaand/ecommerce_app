ac33b927b4609e418db99353863aa548800015d5
Akhil <akhilaand@gmail.com>
2020-11-01 20:39:15 +0530

    final

--- lib/screen/product_screen.dart
+++ lib/screen/product_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // ignore: camel_case_types
 class Product_screen extends StatelessWidget {
@@ -23,35 +24,65 @@ class Product_screen extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: 30,
-              left: -20,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  RawMaterialButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    elevation: 2.0,
-
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-
-                    shape: CircleBorder(),
-                  ),
-
-                  IconButton(
-                      icon: Icon(Icons.shopping_cart,
-                      color: Colors.white,
-                      ),
-                      onPressed: null)
-                ],
+              right: 28,
+              top: 345,
+              child: Container(
+                child: Icon(Icons.favorite_border,
+                size: 20,
+                color: Color(0xff828282),
+                ),
+                width: 40,
+                  height: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  shape: BoxShape.circle
+                ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(right:5.0,top: 7),
+              child: ListTile(
+                leading: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 20.0,
+                      ), onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    ),
+                trailing: Stack(
+                  children:<Widget>[ IconButton(
+                          icon: Icon(Icons.shopping_cart,
+                        color: Colors.white,
+                        ),
+                        onPressed: null),
+                    Positioned(
+                      top: 5,
+                      right: 8,
+                      child: Container(
+                        height: 20,
+                        width:14,
+                        child:  Center(child: Text('2',
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w700
+
+                        ),
+                        )),
+                        decoration: BoxDecoration(
+                            color: Color(0xffF8D9E0),
+                          shape: BoxShape.circle
+                        ),
+
+                      ),
+                    ),
+
+
+                ]),
+              ),
+            ),
+
             Padding(
               padding: const EdgeInsets.only(top:400.0),
               child: Container(
@@ -59,7 +90,8 @@ class Product_screen extends StatelessWidget {
 
                     color: Colors.grey.shade100,
                     borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50))
+                        topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),)
                 ),
 
                 height: MediaQuery.of(context).size.height,
@@ -75,125 +107,181 @@ class Product_screen extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
 
                         children: <Widget>[
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(name,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 25
-                                ),
-                              ),
-                              Text('5 months old',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize:15.5
-                                ),
-                              )
-                            ],
+                          Text(name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 25
+                            ),
                           ),
                           SizedBox(
                             height: 5,
                           ),
 
+                          Text(price,
+                            style: TextStyle(
+                                color: Colors.grey.shade500,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text('Your Size',
+                            style: TextStyle(
+                                color: Colors.grey.shade800,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 17
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
                           Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
-                              Text(price,
-                                style: TextStyle(
-                                    color: Colors.grey.shade500,
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 17
+
+                              Container(
+                                height: 28,
+                                width: 25,
+                                child: Center(
+                                  child: Text('S',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+
+                                        color: Color(0XFF828282),
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14
+                                    ),),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  boxShadow:[
+                                BoxShadow(
+                                color: Colors.grey.shade300,
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  offset: Offset(1, 1),
+                                ),
+
+                                ]),
+                              ),
+                              SizedBox(width: 5,),
+                              Container(
+                                height: 28,
+                                width: 25,
+                                child: Center(
+                                  child: Text('M',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+
+                                        color: Color(0XFF828282),
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14
+                                    ),),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Color(0xffF8D9E0),
+                                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  boxShadow:[
+                                BoxShadow(
+                                color: Colors.grey.shade300,
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  offset: Offset(1, 1),
+                                ),
+
+
+                                ]),
+                              ),
+                              SizedBox(width: 5,),
+                              Container(
+                                height: 28,
+                                width: 25,
+                                child: Center(
+                                  child: Text('L',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+
+                                        color: Color(0XFF828282),
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14
+                                    ),),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  boxShadow:[
+                                BoxShadow(
+                                color: Colors.grey.shade300,
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  offset: Offset(1, 1),
+                                ),
+
+                               ] ),
+                              ),
+                              SizedBox(width: 5,),
+
+                              Container(
+                                height: 28,
+                                width: 25,
+                                child: Center(
+                                  child: Text('XL',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+
+                                        color: Color(0XFF828282),
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14
+                                    ),),
+                                ),
+                                decoration: BoxDecoration(
+                                  boxShadow:[
+                                    BoxShadow(
+                                      color: Colors.grey.shade300,
+                                      spreadRadius: 1,
+                                      blurRadius: 1,
+                                      offset: Offset(1, 1),
+                                    )
+                                  ],
+                                    color: Colors.grey.shade200,
+                                    borderRadius: BorderRadius.all(Radius.circular(5))
                                 ),
                               ),
-                              Text('Female',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize:15.5
-                                ),
-                              )
                             ],
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Divider(
-                            color: Colors.grey.shade300,
-                            thickness: 1,
-                          ),
-                          ListTile(
-                            leading:CircleAvatar(
-                              radius:30,
-//                              backgroundImage: AssetImage('images/circle.jpg'),
-                            ),
-                            title: Text('Donald',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 20
-                              ),
-                            ),
-                            subtitle: Text('owner',
-                              style: TextStyle(
-                                  color: Colors.grey.shade500,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 15
-                              ),
-                            ),
-                            trailing: Wrap(
-                              spacing: 12, // space between two icons
-                              children: <Widget>[
-                                Icon(Icons.location_on,
-                                  color: Colors.black,), // icon-1
-                                Text('1 km',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize:16.5
-                                  ),
-                                )// icon-2
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
 
-                        ],
-                      ),
+
+
+                          SizedBox(
+                            height: 10,
+                          ),
                       Padding(
                         padding: const EdgeInsets.only(bottom: 25),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Container(
-                              height: 40,
-                              width: 50,
-                              child: Icon(Icons.favorite_border,
-                                color: Colors.grey.shade100,
-                              ),
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.all(Radius.circular(10))
-                              ),
-                            ),
 
-                            Container(
-                              height: 40,
-                              width: MediaQuery.of(context).size.width/1.7,
-                              child: Center(
-                                child: Text('GET ME HOME',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 12,
-                                      color: Colors.white
+
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 40,
+                                width: MediaQuery.of(context).size.width,
+                                child: Center(
+                                  child: Text('Add to Cart',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12,
+                                        color: Colors.white
+                                    ),
                                   ),
                                 ),
-                              ),
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
                               ),
                             ),
 
@@ -202,10 +290,10 @@ class Product_screen extends StatelessWidget {
                       )
                     ],
                   ),
-                ),
+               ] ),
               ),
             )
-          ],
+            ) ],
         ),
       ),
     );
